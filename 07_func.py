0f7144b0adb330fcf6c299d5ba53e076934bccf8
danielbatres <danielbatresuser@gmail.com>
2023-10-22 16:12:46 -0600

    Functions

---
+++ 07_func.py
@@ -0,0 +1,17 @@
+print('hello')
+
+def my_print(text):
+  print(text * 2)
+
+my_print('This is my text')
+my_print('Hello')
+
+a = 10
+b = 90
+
+c = a + b
+
+def sum(a, b):
+  my_print(a + b)
+
+sum(1, 5)
