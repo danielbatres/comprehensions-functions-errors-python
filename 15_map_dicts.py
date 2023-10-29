8b0cba181dc9ce9079bc7582d7d7fec879380363
danielbatres <danielbatresuser@gmail.com>
2023-10-29 15:57:32 -0600

    Map with immutability

--- 15_map_dicts.py
+++ 15_map_dicts.py
@@ -9,9 +9,10 @@ prices = list(map(lambda item: item['price'], items))
 print(prices)
 
 def add_taxes(item):
-  item['taxes'] = item['price'] * .19
+  new_item = item.copy()
+  new_item['taxes'] = new_item['price'] * .19
 
-  return item
+  return new_item
 
 new_items = list(map(add_taxes, items))
 
