a64a9c5415bd721f5b8396d68ee25546e2912a2b
danielbatres <danielbatresuser@gmail.com>
2023-10-29 20:32:16 -0600

    Errors in python

---
+++ 21_errors.py
@@ -0,0 +1,16 @@
+# print(0 / 0)
+
+# print(result)
+
+print('Hello')
+
+sum = lambda x, y: x + y
+
+assert sum(2, 2) == 4
+
+print('Hello 2')
+
+age = 10
+
+if age < 18:
+  raise Exception('Minors are not allowed')
